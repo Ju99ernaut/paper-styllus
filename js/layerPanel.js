f0119202243535a5ef8f3f846e8aaf34b9ed4baf
Brendon <brendonzee@gmail.com>
2020-07-22 16:24:13 +0200

    Remove old layer panel and jquery-ui

--- js/layerPanel.js
+++ js/layerPanel.js
@@ -1,38 +1,38 @@
-pg.layerPanel = function() {
-		
-	var toggleVisibility = function() {
+pg.layerPanel = function () {
+
+	var toggleVisibility = function () {
 		var $panel = jQuery('.layerPanel');
-		if(!$panel || $panel.length <= 0) {
+		if (!$panel || $panel.length <= 0) {
 			setup();
-			
+
 		} else {
-			if($panel.hasClass('hidden')) {
+			if ($panel.hasClass('hidden')) {
 				$panel.removeClass('hidden');
 			} else {
 				$panel.addClass('hidden');
 			}
 		}
 	};
-	
-	
-	var setup = function() {
-		loadResources();
-		
+
+
+	var setup = function () {
+		//loadResources();
+
 		var $panel = jQuery('<div class="layerPanel">');
-		var baseTopOffset = jQuery('.appNav').height()+jQuery('.settingsBarContainer').height();
+		var baseTopOffset = jQuery('.appNav').height() + jQuery('.settingsBarContainer').height();
 		$panel.css({
-			'top': baseTopOffset+'px',
-			'max-height': jQuery(window).height()-baseTopOffset+'px' 
+			'top': baseTopOffset + 'px',
+			'max-height': jQuery(window).height() - baseTopOffset + 'px'
 		});
 		var $header = jQuery('<header class="layerPanelHeader"><h2>Layers</h2></header>');
 		var $newLayerButton = jQuery('<button class="newLayerButton">Add</button>');
-		
-		$newLayerButton.click(function() {
+
+		$newLayerButton.click(function () {
 			var newLayer = pg.layer.addNewLayer();
 			newLayer.activate();
 			updateLayerList();
 		});
-		
+
 		$header.append($newLayerButton);
 
 		var $layerEntries = jQuery('<div class="layerEntries">');
@@ -41,76 +41,76 @@ pg.layerPanel = function() {
 			forcePlaceholderSize: true,
 			tolerance: 'pointer',
 			delay: 300,
-			stop: function(event, ui) {
+			stop: function (event, ui) {
 				handleLayerOrderChange();
 			}
 		});
-		
-		$panel.append($header,$layerEntries);
+
+		$panel.append($header, $layerEntries);
 		jQuery('body').append($panel);
-		
+
 		$layerEntries.css({
-			'max-height': jQuery('body').height() - baseTopOffset - $header.height()+'px'
+			'max-height': jQuery('body').height() - baseTopOffset - $header.height() + 'px'
 		});
 
 		updateLayerList();
-		
+
 		updateLayerValues();
-		
-		jQuery(document).on('LayerAdded LayerRemoved', function() {
+
+		jQuery(document).on('LayerAdded LayerRemoved', function () {
 			updateLayerList();
 		});
-		
-		jQuery(document).on('DocumentUpdate', function(){
+
+		jQuery(document).on('DocumentUpdate', function () {
 			updateLayerValues();
 		});
 	};
-	
-	
-	var setupLayerEntry = function(layer) {
-		if(!(layer.data && layer.data.isGuideLayer)) {
+
+
+	var setupLayerEntry = function (layer) {
+		if (!(layer.data && layer.data.isGuideLayer)) {
 			var $activeClass = '';
-			if(pg.layer.isActiveLayer(layer)) {
-				$activeClass= ' active';
+			if (pg.layer.isActiveLayer(layer)) {
+				$activeClass = ' active';
 			}
-			var $layerEntry = jQuery('<ul class="layerEntry'+$activeClass+'" data-layer-id="'+layer.data.id+'">');
+			var $layerEntry = jQuery('<ul class="layerEntry' + $activeClass + '" data-layer-id="' + layer.data.id + '">');
 			var $layerVisSection = jQuery('<li class="layerVisSection">');
 			var $layerVisButton = jQuery('<input type="checkbox" class="layerVisibilityToggle" title="Layer visibility">').attr('checked', layer.visible);
 			var $layerNameSection = jQuery('<li class="layerName" title="">');
 			var $layerNameInput = jQuery('<input type="text">').val(layer.name);
 			var $layerActionSection = jQuery('<li class="layerActions">');
-			var $layerDeleteButton = jQuery('<button class="layerDeleteButton" data-layer-id="'+layer.data.id+'" title="Delete layer">&times;</button>');
+			var $layerDeleteButton = jQuery('<button class="layerDeleteButton" data-layer-id="' + layer.data.id + '" title="Delete layer">&times;</button>');
 			var $layerInfo = jQuery('<li class="layerInfo" title="Selected 0/0 Total">i</li>');
 			var $layerSelectSection = jQuery('<li class="layerSelectSection">');
 			var $layerSelectButton = jQuery('<input type="radio" class="layerSelectToggle" title="Select all/none">');
-			
-			$layerEntry.click(function() {
+
+			$layerEntry.click(function () {
 				setActiveLayerEntry(layer);
 			});
-			
-			$layerVisButton.click(function() {
+
+			$layerVisButton.click(function () {
 				layer.visible = !layer.visible;
 			});
-			
-			
-			if(layer.data.isDefaultLayer) {
+
+
+			if (layer.data.isDefaultLayer) {
 				$layerNameInput.attr('disabled', true);
 			}
-			
-			
-			$layerNameInput.on('change', function() {
+
+
+			$layerNameInput.on('change', function () {
 				layer.name = jQuery(this).val();
 			});
-			
-			$layerDeleteButton.click(function() {
-				if(confirm('Delete this layer and all its children?')) {
+
+			$layerDeleteButton.click(function () {
+				if (confirm('Delete this layer and all its children?')) {
 					pg.layer.deleteLayer(jQuery(this).attr('data-layer-id'));
 					updateLayerList();
 				};
 			});
 
-			$layerSelectButton.click(function() {
-				if(jQuery(this).attr('checked')) {
+			$layerSelectButton.click(function () {
+				if (jQuery(this).attr('checked')) {
 					pg.selection.clearSelection();
 					jQuery(this).removeAttr('checked');
 
@@ -118,16 +118,16 @@ pg.layerPanel = function() {
 					pg.selection.clearSelection();
 
 					var items = pg.helper.getPaperItemsByLayerID(layer.data.id);
-					jQuery.each(items, function(index, item) {
+					jQuery.each(items, function (index, item) {
 						pg.selection.setItemSelection(item, true);
 					});
-					jQuery(this).attr('checked', items.length >0);
+					jQuery(this).attr('checked', items.length > 0);
 				}
 			});
 
 			$layerVisSection.append($layerVisButton);
 			$layerNameSection.append($layerNameInput);
-			if(!layer.data.isDefaultLayer) {
+			if (!layer.data.isDefaultLayer) {
 				$layerActionSection.append($layerDeleteButton);
 			}
 			$layerSelectSection.append($layerSelectButton);
@@ -135,49 +135,49 @@ pg.layerPanel = function() {
 			jQuery('.layerEntries').prepend($layerEntry);
 		}
 	};
-	
-	
-	var setActiveLayerEntry = function(layer) {
+
+
+	var setActiveLayerEntry = function (layer) {
 		jQuery('.layerEntry').removeClass('active');
 		pg.layer.setActiveLayer(layer);
-		jQuery('.layerEntry[data-layer-id="'+layer.data.id+'"]').addClass('active');
+		jQuery('.layerEntry[data-layer-id="' + layer.data.id + '"]').addClass('active');
 	};
-	
-	
-	var handleLayerOrderChange = function() {
+
+
+	var handleLayerOrderChange = function () {
 		var order = [];
-		jQuery('.layerEntries').children().each(function() {
+		jQuery('.layerEntries').children().each(function () {
 			order.push(jQuery(this).attr('data-layer-id'));
 		});
 		pg.layer.changeLayerOrderByIDArray(order);
 	};
-	
-	
-	var updateLayerList = function() {
+
+
+	var updateLayerList = function () {
 		jQuery('.layerEntries').empty();
-		jQuery.each(paper.project.layers, function(index, layer) {
+		jQuery.each(paper.project.layers, function (index, layer) {
 			setupLayerEntry(layer);
 		});
-		
+
 	};
-	
-	var updateLayerValues = function() {
-		jQuery('.layerEntry').each(function() {
+
+	var updateLayerValues = function () {
+		jQuery('.layerEntry').each(function () {
 			var id = parseInt(jQuery(this).attr('data-layer-id'));
 			var layer = pg.layer.getLayerByID(id);
-			if(layer) {
-				
+			if (layer) {
+
 				var selectedItems = 0;
-				jQuery.each(layer.children, function(index, child) {
-					if(child.selected || child.fullySelected) {
+				jQuery.each(layer.children, function (index, child) {
+					if (child.selected || child.fullySelected) {
 						selectedItems++;
 					}
 				});
 
 				var $entry = jQuery(this);
-				$entry.find('.layerInfo').attr('title','Selected '+selectedItems+'/'+layer.children.length+' Total');
+				$entry.find('.layerInfo').attr('title', 'Selected ' + selectedItems + '/' + layer.children.length + ' Total');
 
-				if(layer.children.length > 0 && selectedItems === layer.children.length) {
+				if (layer.children.length > 0 && selectedItems === layer.children.length) {
 					$entry.find('.layerSelectToggle').prop('checked', true);
 				} else {
 					$entry.find('.layerSelectToggle').prop('checked', false);
@@ -185,24 +185,24 @@ pg.layerPanel = function() {
 			}
 		});
 	};
-	
-	
-	var loadResources = function() {
-		if(!jQuery('#layerPanelCSS').exists()) {
+
+
+	var loadResources = function () {
+		if (!jQuery('#layerPanelCSS').exists()) {
 			jQuery("<link />", {
 				href: "css/layerPanel.css",
 				rel: "stylesheet",
 				id: "layerPanelCSS"
-			}).appendTo("head", function() {
+			}).appendTo("head", function () {
 				return true;
 			});
 		};
 	};
 
-	
+
 	return {
-		toggleVisibility:toggleVisibility,
-		updateLayerList:updateLayerList
+		toggleVisibility: toggleVisibility,
+		updateLayerList: updateLayerList
 	};
-	
+
 }();
